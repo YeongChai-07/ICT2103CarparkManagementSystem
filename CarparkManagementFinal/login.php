c1c1d3b75aa2923bd8a0ce31139b66ec5be59550
LT Tang <skytanglitian@gmail.com>
2016-11-18 03:25:23 +0800

    Login/out completed

--- CarparkManagementFinal/login.php
+++ CarparkManagementFinal/login.php
@@ -4,26 +4,21 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="newcss.css">
+        <link href="css/newcss.css" rel="stylesheet" type="text/css"/>
         <script src="loginJS.js"></script>
     </head>
-    <body>
+
+    <body background="img/bg/carparkHomepage.jpg" >
         <div class="container">
             <div class="row">
                 <div class="col-md-6 col-md-offset-3">
                     <h2>Carpark Management</h2>
                     <div class="panel panel-login">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <!--                                <div class="col-xs-6">
-                                                                    <a href="#" class="active" id="login-form-link">Login</a>
-                                                                </div>
-                                                                <div class="col-xs-6">
-                                                                    <a href="#" id="register-form-link">Register</a>
-                                                                </div>-->
-                            </div>
-                            <hr>
+                        <div class="row">
+
                         </div>
+                        <hr>
+
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-lg-12">
@@ -35,7 +30,7 @@
                                             <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
                                         </div>
                                         <div class="form-group text-center">
-                                            <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+                                            <input type="checkbox" tabindex="3" class="whiteLabel" name="remember" id="remember">
                                             <label for="remember"> Remember Me</label>
                                         </div>
                                         <div class="form-group">
